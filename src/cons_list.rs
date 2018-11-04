578ee28d7d030d5d5db113191b626703b4774fe9
Nikos Filippakis <aesmade@gmail.com>
2018-11-04 12:02:10 +0100

    Impl Drop
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- src/cons_list.rs
+++ src/cons_list.rs
@@ -22,6 +22,26 @@ impl<T> ConsList<T> {
         let new = Node{elem: val, next: old_head};
         self.0 = Link::More(Box::new(new));
     }
+
+    pub fn pop(&mut self) -> Option<T> {
+        match mem::replace(&mut self.0, Link::None){
+            Link::More(node) => {
+                let node = *node;
+                self.0 = node.next;
+                Some(node.elem)
+            },
+            Link::None => None,
+        }
+    }
+}
+
+impl<T> Drop for ConsList<T> {
+    fn drop(&mut self) {
+        let mut link = mem::replace(&mut self.0, Link::None);
+        while let Link::More(mut next_node) = link {
+            link = mem::replace(&mut next_node.next, Link::None);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -38,14 +58,26 @@ mod tests {
 
     #[test]
     fn push_front_int() {
-        let mut new: ConsList<i32> = ConsList::new();
+        let mut new = ConsList::new();
         new.push(10);
         new.push(20);
         new.push(30);
-        if let Link::More(first_node) = new.0 {
+        if let Link::More(ref first_node) = new.0 {
             assert_eq!(first_node.elem, 30);
         } else {
             panic!("No first node");
         }
+    }
+
+    #[test]
+    fn pop_ints() {
+        let mut new = ConsList::new();
+        new.push(String::from("a"));
+        new.push(String::from("b"));
+        new.push(String::from("c"));
+        assert_eq!(new.pop(), Some(String::from("c")));
+        assert_eq!(new.pop(), Some(String::from("b")));
+        assert_eq!(new.pop(), Some(String::from("a")));
+        assert_eq!(new.pop(), None);
     }
 }
