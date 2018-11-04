8ae0969810d19925dc1e24453329e6fe3df8783b
Nikos Filippakis <aesmade@gmail.com>
2018-11-04 11:41:33 +0100

    Stack push
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- src/cons_list.rs
+++ src/cons_list.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub enum Link<T> {
     More(Box<Node<T>>),
     None,
@@ -11,8 +13,14 @@ pub struct Node<T> {
 pub struct ConsList<T>(Link<T>);
 
 impl<T> ConsList<T> {
-    pub fn new(val: T) -> ConsList<T> {
-        ConsList(Link::More(Box::new(Node{elem: val, next: Link::None})))
+    pub fn new() -> ConsList<T> {
+        ConsList(Link::None)
+    }
+
+    pub fn push(&mut self, val: T) {
+        let old_head = mem::replace(&mut self.0, Link::None);
+        let new = Node{elem: val, next: old_head};
+        self.0 = Link::More(Box::new(new));
     }
 }
 
@@ -22,11 +30,22 @@ mod tests {
 
     #[test]
     fn create_new() {
-        let new = ConsList::new(10);
-        if let ConsList(Link::More(node_box)) = new {
-            assert_eq!(node_box.elem, 10);
+        let new: ConsList<i32> = ConsList::new();
+        if let Link::More(_) = new.0 {
+            panic!("Should be none");
+        }
+    }
+
+    #[test]
+    fn push_front_int() {
+        let mut new: ConsList<i32> = ConsList::new();
+        new.push(10);
+        new.push(20);
+        new.push(30);
+        if let Link::More(first_node) = new.0 {
+            assert_eq!(first_node.elem, 30);
         } else {
-            panic!("Wrong type of ConsList enum");
+            panic!("No first node");
         }
     }
 }
