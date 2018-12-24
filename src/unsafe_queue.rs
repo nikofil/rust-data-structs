254d34b371f314947fc5487e82e8a74ec440b771
Nikos Filippakis <aesmade@gmail.com>
2018-12-24 19:52:03 +0100

    Unsafe queue iters
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- src/unsafe_queue.rs
+++ src/unsafe_queue.rs
@@ -44,6 +44,62 @@ impl<T> Queue<T> {
             h.elem
         })
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter{ que: self }
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter{ cur: self.head.as_ref() }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut{ cur: self.head.as_mut() }
+    }
+}
+
+pub struct IntoIter<T> {
+    que: Queue<T>,
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        self.que.pop()
+    }
+}
+
+pub struct Iter<'a, T: 'a> {
+    cur: Option<&'a Box<Node<T>>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        self.cur.take().map(|c| {
+            let c = &**c;
+            self.cur = c.next.as_ref();
+            &c.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T: 'a> {
+    cur: Option<&'a mut Box<Node<T>>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        self.cur.take().map(|c| {
+            let c = &mut **c;
+            self.cur = c.next.as_mut();
+            &mut c.elem
+        })
+    }
 }
 
 #[cfg(test)]
@@ -58,5 +114,27 @@ mod tests {
         assert_eq!(que.pop().unwrap(), String::from("a"));
         assert_eq!(que.pop().unwrap(), String::from("b"));
         assert!(que.pop().is_none());
+    }
+
+    #[test]
+    fn test_iters() {
+        let mut que = Queue::new();
+        let v = vec![String::from("a"), String::from("b"), String::from("c")];
+        for i in &v {
+            que.push(i.clone());
+        }
+        for i in que.iter_mut() {
+            let is = i.clone();
+            i.push_str(&is);
+        }
+        for (i, s) in que.iter().enumerate() {
+            let mut tmp = String::new();
+            tmp.push_str(&v[i]);
+            tmp.push_str(&v[i]);
+            assert_eq!(&tmp, s);
+        }
+        for i in que.into_iter() {
+            assert_eq!(i.len(), 2);
+        }
     }
 }
